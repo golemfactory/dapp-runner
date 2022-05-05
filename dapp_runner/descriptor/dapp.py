3c2359b25fdab944dcf1b26e52755d2955b7466e
shadeofblue <blue@golem.network>
2022-05-05 11:17:12 +0200

    http proxy parsing + example descriptors using the local http proxy

--- dapp_runner/descriptor/dapp.py
+++ dapp_runner/descriptor/dapp.py
@@ -1,7 +1,7 @@
 """Class definitions for the Dapp Runner's dapp descriptor."""
 from dataclasses import dataclass, field
 
-from typing import Dict, Union, List, Any
+from typing import Dict, Union, List, Any, Optional
 
 from .base import BaseDescriptor
 
@@ -12,6 +12,28 @@ class PayloadDescriptor:
 
     runtime: str
     params: Dict[str, Any] = field(default_factory=dict)
+
+
+@dataclass
+class PortMapping:
+    """Port mapping for a http proxy."""
+
+    remote_port: int
+    local_port: Optional[int] = None
+
+
+@dataclass
+class HttpProxy(BaseDescriptor["HttpProxy"]):
+    """HTTP Proxy descriptor."""
+
+    def __ports_factory(value: str) -> PortMapping:  # type: ignore [misc]  # noqa
+        ports = [int(p) for p in value.split(":")]
+        port_mappping = PortMapping(remote_port=ports.pop())
+        if ports:
+            port_mappping.local_port = ports.pop()
+        return port_mappping
+
+    ports: List[PortMapping] = field(metadata={"factory": __ports_factory})
 
 
 @dataclass
@@ -27,6 +49,7 @@ class ServiceDescriptor(BaseDescriptor["ServiceDescriptor"]):
 
     payload: str
     entrypoint: List[List[str]] = field(metadata={"factory": __entrypoint_factory})
+    http_proxy: Optional[HttpProxy] = None
 
 
 @dataclass
