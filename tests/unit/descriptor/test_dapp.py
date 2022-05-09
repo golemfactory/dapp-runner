939c3140018ccf46949d4c1c07f48b532b129f15
shadeofblue <blue@golem.network>
2022-05-09 10:12:29 +0200

    reinstate the "unknown payload" test
    + implicit network test

--- tests/unit/descriptor/test_dapp.py
+++ tests/unit/descriptor/test_dapp.py
@@ -6,6 +6,8 @@ from dapp_runner.descriptor.dapp import (
     PayloadDescriptor,
     ServiceDescriptor,
     HttpProxyDescriptor,
+    VM_PAYLOAD_CAPS_KWARG,
+    vm,
 )
 
 
@@ -33,6 +35,25 @@ from dapp_runner.descriptor.dapp import (
         ),
         (
             {
+                "payloads": {
+                    "simple-service": {
+                        "runtime": "vm",
+                        "params": {"image_hash": "some-hash"},
+                    }
+                },
+                "nodes": {
+                    "simple-service": {
+                        "payload": "other",
+                        "entrypoint": [
+                            ["/golem/run/simulate_observations_ctl.py", "--start"],
+                        ],
+                    }
+                },
+            },
+            DescriptorError("Undefined payload: `other`"),
+        ),
+        (
+            {
                 "nodes": {
                     "simple-service": {
                         "payload": "simple-service",
@@ -56,8 +77,8 @@ def test_dapp_descriptor(descriptor_dict, error, test_utils):
     """Test whether the Dapp descriptor loads correctly."""
     try:
         dapp = DappDescriptor.load(descriptor_dict)
-        payload = dapp.payloads[list(dapp.payloads.keys())[0]]
-        service = dapp.nodes[list(dapp.nodes.keys())[0]]
+        payload = list(dapp.payloads.values())[0]
+        service = list(dapp.nodes.values())[0]
         assert isinstance(payload, PayloadDescriptor)
         assert isinstance(service, ServiceDescriptor)
     except Exception as e:  # noqa
@@ -67,11 +88,11 @@ def test_dapp_descriptor(descriptor_dict, error, test_utils):
 
 
 @pytest.mark.parametrize(
-    "descriptor_dict, remote_port, local_port, error",
+    "descriptor_dict, remote_port, local_port, error, implicit_vpn",
     [
         (
             {
-                "payloads": {},
+                "payloads": {"foo": {"runtime": "bar"}},
                 "nodes": {
                     "http": {
                         "payload": "foo",
@@ -87,10 +108,11 @@ def test_dapp_descriptor(descriptor_dict, error, test_utils):
             25,
             2525,
             None,
+            False,
         ),
         (
             {
-                "payloads": {},
+                "payloads": {"foo": {"runtime": "bar"}},
                 "nodes": {
                     "http": {
                         "payload": "foo",
@@ -106,20 +128,51 @@ def test_dapp_descriptor(descriptor_dict, error, test_utils):
             80,
             None,
             None,
+            False,
+        ),
+        (
+            {
+                "payloads": {"foo": {"runtime": "vm"}},
+                "nodes": {
+                    "http": {
+                        "payload": "foo",
+                        "entrypoint": [],
+                        "http_proxy": {
+                            "ports": [
+                                "80",
+                            ]
+                        },
+                    }
+                },
+            },
+            80,
+            None,
+            None,
+            True,
         ),
     ],
 )
 def test_http_proxy_descriptor(
-    descriptor_dict, remote_port, local_port, error, test_utils
+    descriptor_dict, remote_port, local_port, error, test_utils, implicit_vpn
 ):
     """Test whether the `http_proxy` descriptor is correctly interpreted."""
     try:
         dapp = DappDescriptor.load(descriptor_dict)
-        service = dapp.nodes[list(dapp.nodes.keys())[0]]
+        service = list(dapp.nodes.values())[0]
         assert isinstance(service.http_proxy, HttpProxyDescriptor)
         ports = service.http_proxy.ports[0]
         assert ports.local_port == local_port
         assert ports.remote_port == remote_port
+
+        # check implicit network initialization
+        assert service.network
+
+        # check implicit VPN capability for a VM runtime
+        if implicit_vpn:
+            payload = list(dapp.payloads.values())[0]
+            assert VM_PAYLOAD_CAPS_KWARG in payload.params
+            assert vm.VM_CAPS_VPN in payload.params[VM_PAYLOAD_CAPS_KWARG]
+
     except Exception as e:  # noqa
         test_utils.verify_error(error, e)
     else:
