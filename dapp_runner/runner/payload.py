ad08ecfb69bc095ad1be2dd0b61721b3116790f7
shadeofblue <blue@wave460.net>
2022-09-20 15:09:27 +0200

    add support for vm/manifest payloads

--- dapp_runner/runner/payload.py
+++ dapp_runner/runner/payload.py
@@ -5,17 +5,22 @@ from typing import Dict, Union, Callable, Awaitable
 
 from yapapi.payload import vm, Payload
 
-from dapp_runner.descriptor.dapp import PayloadDescriptor
+from dapp_runner.descriptor.dapp import (
+    PayloadDescriptor,
+    PAYLOAD_RUNTIME_VM,
+    PAYLOAD_RUNTIME_VM_MANIFEST,
+)
 from .error import RunnerError
 
 
 async def get_payload(desc: PayloadDescriptor) -> Payload:
     """Create an instance of yapapi Payload for a given runtime type."""
     runtimes: Dict[str, Callable[..., Union[Payload, Awaitable[Payload]]]] = {
-        "vm": vm.repo
+        PAYLOAD_RUNTIME_VM: vm.repo,
+        PAYLOAD_RUNTIME_VM_MANIFEST: vm.manifest,
     }
 
-    if desc.runtime in runtimes.keys():
+    if desc.runtime.lower() in runtimes.keys():
         payload = runtimes[desc.runtime](**desc.params)
         if inspect.isawaitable(payload):
             return await payload
