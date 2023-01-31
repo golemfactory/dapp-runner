2558c8165f4025d63cce653c3fee6b8224dac70b
Lucjan Dudek <lucekdudek@gmail.com>
2023-01-31 09:53:34 +0100

    Fix old utils tests

--- tests/unit/util/test_util.py
+++ tests/unit/util/test_util.py
@@ -1,21 +1,22 @@
 """Unit tests for dapp_runner._util."""
 from unittest import mock
 
+import pytest
+
 from dapp_runner._util import get_free_port
 
 
-@mock.patch("socket.socket.connect_ex", mock.Mock(side_effect=[1, 0]))
+@mock.patch("socket.socket.bind", mock.Mock(side_effect=[OSError, None]))
 def test_get_free_port_available():
     """Test if the first available port is correctly returned."""
     assert get_free_port() == 8081
 
 
-@mock.patch("socket.socket.connect_ex", mock.Mock(return_value=1))
+@mock.patch("socket.socket.bind", mock.Mock(side_effect=OSError))
 def test_get_free_port_exceeded(test_utils):
     """Test if the expected error is raised when no free port was found."""
-    try:
+    with pytest.raises(RuntimeError) as e:
         get_free_port()
-    except Exception as e:
         test_utils.verify_error(
             RuntimeError("No free ports found. range_start=8080, range_end=9090"), e
         )
